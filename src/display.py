302496dd60ee4c1dc3786a7a380d4891f6c58146
Alejandro Rodríguez <omar.alejandro.rodriguez@gmail.com>
2023-12-13 21:06:11 +0100

    functional version on Windows

--- src/display.py
+++ src/display.py
@@ -1,7 +1,6 @@
 import curses
-import time
 import cv2
-
+N_FRAMES = 500
 class LetterDisplay:
     def __init__(self):
         #get default video source
@@ -14,17 +13,13 @@ class LetterDisplay:
         self.gradient_string = " .:-=+*#%@"
 
     def start(self):
-        for c in range(0, 500):
+        for c in range(0, N_FRAMES):
             #get 1 video frame
             ret, frame = self.video_source.read()
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            #self.stdscr.addstr(0,10,"orig size {} {}".format(c, frame.shape))
             frame = self.scale(frame=frame)
-            #self.stdscr.addstr(1,10,"resized {} {}".format(c, frame.shape))
-            #self.stdscr.addstr(2,0,"sdf {} {}".format(frame[10,10], self.gradient_string[3]))
             self.draw(frame=frame)
             self.stdscr.refresh()
-            #time.sleep(0.5)
 
 
 
@@ -35,15 +30,7 @@ class LetterDisplay:
         curses.endwin()
 
     def scale(self, frame):
-        #based on https://stackoverflow.com/a/64884087
         max_height, max_width =  self.stdscr.getmaxyx()
-        '''
-        height, width = frame.shape[:2]
-        f1 = max_width / width
-        f2 = max_height / height
-        f = min(f1, f2)  # resizing factor
-        dim = (int(frame.shape[1] * f), int(frame.shape[0] * f))
-        return cv2.resize(frame, dim)'''
         return cv2.resize(frame, (max_width, max_height))
 
 
